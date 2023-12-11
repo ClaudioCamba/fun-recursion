a6182ccd3f2f9c814fc2180005a24cbd3a771252
Ward Sarkhan <Ward@ward.local>
2023-12-11 12:39:42 +0000

    committed

--- post-lecture/fun-recursion.js
+++ post-lecture/fun-recursion.js
@@ -1,11 +1,33 @@
 function reverseStr(string){
-const reversedStr = [];
-
-for (let i = string.length-1; i !== -1; i--){
-    reversedStr.push(string[i]);
+    if(string.length === 0){
+        return ''
+       }
+   return reverseStr(string.substring(1))+string.charAt(0)
 }
 
-return reversedStr.join('');
+function sumDigits(number) {
+   let numberArray = number.toString().split('')
+   console.log(numberArray)
+   if (numberArray.length === 1) {
+    return number
+   }
+   else {
+    return sumDigits(Number(numberArray[0])+Number(numberArray[1]))
+   }
 }
 
-module.exports = {reverseStr}
+// needs to run the times of the number given 
+// needs to know the previous numbers 
+// needs to add the previus two numbers together
+
+function fib(number){
+    if (number === 1) return 1;
+        return number-2 + fib(number-1)
+    }
+    
+
+
+
+
+
+module.exports = {reverseStr, sumDigits, fib}
--- post-lecture/fun-recursion.test.js
+++ post-lecture/fun-recursion.test.js
@@ -1,4 +1,4 @@
-const {reverseStr} = require('./fun-recursion')
+const {reverseStr, sumDigits, fib} = require('./fun-recursion')
 
 describe('reverseStr', () => {
     test('Should reverse a string', ()=>{
@@ -21,5 +21,50 @@ describe('reverseStr', () => {
         const output = reverseStr('katherine rules')
         //Assertion
         expect(output).toEqual('selur enirehtak');
+    });
+});
+
+describe('sumDigits', () => {
+    test('if a single digit returns the digit', ()=>{
+        //Arrange
+        //Act
+        const output = sumDigits(1)
+        //Assertion
+        expect().toBe();
+        expect(output).toEqual(1);
+    });
+    test('should return sum of digits of a number until only one digit remains', ()=>{
+        //Arrange
+        //Act
+        const output = sumDigits(99)
+        //Assertion
+        expect().toBe();
+        expect(output).toEqual(9);
+    });
+    test('should return sum of digits of a number until only one digit remains', ()=>{
+        //Arrange
+        //Act
+        const output = sumDigits(999)
+        //Assertion
+        expect().toBe();
+        expect(output).toEqual(9);
+    });
+    
+});
+
+describe.only('fib', () => {
+    test('takes 1 and returns the number at that point in the fibonacci sequence', ()=>{
+        //Arrange
+        //Act
+        const output = fib(1)
+        //Assertion
+        expect(output).toEqual(0);
+    });
+    test('takes a number and returns the number at that point in the fibonacci sequence', ()=>{
+        //Arrange
+        //Act
+        const output = fib(2)
+        //Assertion
+        expect(output).toEqual(1);
     });
 });
